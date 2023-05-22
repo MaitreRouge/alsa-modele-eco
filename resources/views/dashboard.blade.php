3979cc71d567adf5787ff25becea79e5403e26c6
MaitreRouge <marc.magueur@outlook.fr>
2023-05-22 16:48:05 +0200

    ✏️ Oubli du texte descriptif

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,7 +25,7 @@
         <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto">
                 <h1 class="text-base font-semibold leading-6 text-gray-900">Devis</h1>
-                <p class="mt-2 text-sm text-gray-700">Ici, vous retrouverez .</p>
+                <p class="mt-2 text-sm text-gray-700">Ici, vous retrouverez tous les devis de tous les clients et j'ai la flemme de faire une option pour trier par client :D.</p>
             </div>
             <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
                 <a type="button" href="/new" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Créer un nouveau devis</a>
@@ -66,7 +66,7 @@
                             </td>
 {{--                            <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">Member</td>--}}
                             <td class="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                                <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                <a href="/edit/{{ $client->id }}" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                             </td>
                         </tr>
                         @endforeach
