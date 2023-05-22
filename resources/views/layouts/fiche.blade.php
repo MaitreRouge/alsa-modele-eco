8ec095cd01a269c91b86f0dc022b1ce0ff614720
MaitreRouge <marc.magueur@outlook.fr>
2023-05-22 16:48:43 +0200

    🏗️ Creation d'une template pour les fiches

--- resources/views/layouts/fiche.blade.php
+++ resources/views/layouts/fiche.blade.php
@@ -1,24 +1,10 @@
 @extends("layouts.layout",
 [
-    "pageTitle" => "Creation devis",
+    "pageTitle" => $pageTitle??"Sans titre",
     "activate" => 0
 ])
 @section("main")
-<!--
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<div>
+<div class="mb-6">
     <div class="hidden sm:block">
         <nav class="isolate flex divide-x divide-gray-200 rounded-lg shadow" aria-label="Tabs">
             <!-- Current: "text-gray-900", Default: "text-gray-500 hover:text-gray-700" -->
@@ -42,27 +28,10 @@
                 <span>Billing</span>
                 <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
             </a>
-            <a href="#" class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
-                <span>Billing</span>
-                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-            </a>
-            <a href="#" class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
-                <span>Billing</span>
-                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-            </a>
-            <a href="#" class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
-                <span>Billing</span>
-                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-            </a>
-            <a href="#" class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
-                <span>Billing</span>
-                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-            </a>
-            <a href="#" class="text-gray-500 hover:text-gray-700 rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
-                <span>Billing</span>
-                <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
-            </a>
         </nav>
     </div>
 </div>
+
+@yield("fiche")
+
 @endsection
