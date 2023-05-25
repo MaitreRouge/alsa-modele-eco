ba1411689d74cb76f5eade47e973d01b9d6fe1ee
MaitreRouge <marc.magueur@outlook.fr>
2023-05-25 10:48:46 +0200

    💄 La catégorie selectionée reste affichée (liste)

--- resources/views/fiches/liste-prestations.blade.php
+++ resources/views/fiches/liste-prestations.blade.php
@@ -17,7 +17,7 @@
                         <select id="tri" name="tri"
                                 class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             @foreach($categories as $c)
-                                <option value="{{ $c->id }}">{{ $c->label }}</option>
+                                <option value="{{ $c->id }}" {{ ($main->id??0 == $c->id)?"selected":"" }}>{{ $c->label }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -82,7 +82,7 @@
                             @foreach($parents as $parent)
                                 <tr class="border-t border-gray-200">
                                     <th colspan="5" scope="colgroup"
-                                        class="bg-gray-50 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                                        class="bg-gray-200 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
                                         {{ $parent->label }}
                                         @if(!empty($parent->note))
                                             <svg class="h-5 w-5 text-yellow-400" style="display: inline" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -96,13 +96,6 @@
                                         <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
                                             {{ $prestation->label }}
                                             @if(!empty($prestation->note))
-
-{{--                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">--}}
-{{--                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />--}}
-{{--                                                </svg>--}}
-
-
-
                                                 <svg class="h-5 w-5 text-purple-500" style="display: inline" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
                                                 </svg>
