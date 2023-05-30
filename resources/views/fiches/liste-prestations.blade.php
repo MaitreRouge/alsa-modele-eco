287b460bcb5e072fcec6a1586f09998e6491fadc
MaitreRouge <marc.magueur@outlook.fr>
2023-05-30 10:51:17 +0200

    🩹 Fix #12

--- resources/views/fiches/liste-prestations.blade.php
+++ resources/views/fiches/liste-prestations.blade.php
@@ -18,7 +18,7 @@
                         <select id="tri" name="tri"
                                 class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                             @foreach($categories as $c)
-                                <option value="{{ $c->id }}" {{ ($main->id??0 == $c->id)?"selected":"" }}>{{ $c->label }}</option>
+                                <option value="{{ $c->id }}" {{ (($main->id??0) == $c->id) ?"selected":"" }}>{{ $c->label }}</option>
                             @endforeach
                         </select>
                     </div>
