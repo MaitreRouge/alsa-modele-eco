c8e5f707f8aed94fe5f4aa6687affd6c75f13f32
MaitreRouge <marc.magueur@outlook.fr>
2023-06-13 16:41:18 +0200

    🩹 Fix notification overlay issue (Fix #29)

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -39,22 +39,20 @@ class Notification extends Model
 
         return '
         <!-- Global notification live region, render this permanently at the end of the document -->
-<div id="notif-'. $this->id .'" aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
-  <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
-    <div class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+    <div class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 z-50" id="notif-' . $this->id . '">
       <div class="p-4">
         <div class="flex items-start">
           <div class="flex-shrink-0">
-            <svg class="h-6 w-6 text-'. $this->color .'-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              '. $svg .'
+            <svg class="h-6 w-6 text-' . $this->color . '-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              ' . $svg . '
             </svg>
           </div>
           <div class="ml-3 w-0 flex-1 pt-0.5">
-            <p class="text-sm font-medium text-gray-900">'. $this->title .'</p>
-            <p class="mt-1 text-sm text-gray-500">'. $this->description .'</p>
+            <p class="text-sm font-medium text-gray-900">' . $this->title . '</p>
+            <p class="mt-1 text-sm text-gray-500">' . $this->description . '</p>
           </div>
           <div class="ml-4 flex flex-shrink-0">
-            <button type="button" id="notif-'. $this->id .'-button" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            <button type="button" id="notif-' . $this->id . '-button" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
               <span class="sr-only">Close</span>
               <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
@@ -64,17 +62,17 @@ class Notification extends Model
         </div>
       </div>
     </div>
-  </div>
-</div>
+
+<div class="text-green-400" style="display: none;"></div>
 
 <script>
     $(document).ready(function () {
-        $("#notif-'. $this->id .'-button").click(function() {
+        $("#notif-' . $this->id . '-button").click(function() {
             hide();
         });
 
         function hide() {
-            $("#notif-'. $this->id .'").hide("fast")
+            $("#notif-' . $this->id . '").hide("fast")
         }
 
         setTimeout(hide, 5000);
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,32 +5,31 @@
 </head>
 <body class="h-full">
 <div class="min-h-full">
-    <div class="min-h-full">
-        <div class="bg-gray-800 pb-32">
-            <nav class="bg-gray-800">
-                <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div class="border-b border-gray-700">
-                        <div class="flex h-16 items-center justify-between px-4 sm:px-0">
-                            <div class="flex items-center">
-                                <div class="flex-shrink-0">
-                                    <img class="h-8 w-8"
-                                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                                         alt="Your Company">
-                                </div>
-                                <div class="hidden md:block">
-                                    <div class="ml-10 flex items-baseline space-x-4">
-                                        <a href="/dashboard"
-                                           class="{{ ($activate == 1)?"bg-gray-900 text-white":"text-gray-300" }} hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
-                                        <a href="/prestations"
-                                           class="{{ ($activate == 2)?"bg-gray-900 text-white":"text-gray-300" }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Prestations</a>
-                                        <a href="/changelog"
-                                           class="{{ ($activate == 3)?"bg-gray-900 text-white":"text-gray-300" }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Changelog</a>
-                                    </div>
+    <div class="bg-gray-800 pb-32">
+        <nav class="bg-gray-800">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="border-b border-gray-700">
+                    <div class="flex h-16 items-center justify-between px-4 sm:px-0">
+                        <div class="flex items-center">
+                            <div class="flex-shrink-0">
+                                <img class="h-8 w-8"
+                                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                                     alt="Your Company">
+                            </div>
+                            <div class="hidden md:block">
+                                <div class="ml-10 flex items-baseline space-x-4">
+                                    <a href="/dashboard"
+                                       class="{{ ($activate == 1)?"bg-gray-900 text-white":"text-gray-300" }} hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium">Dashboard</a>
+                                    <a href="/prestations"
+                                       class="{{ ($activate == 2)?"bg-gray-900 text-white":"text-gray-300" }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Prestations</a>
+                                    <a href="/changelog"
+                                       class="{{ ($activate == 3)?"bg-gray-900 text-white":"text-gray-300" }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Changelog</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
 
             </nav>
             <header class="py-10">
@@ -40,49 +39,52 @@
             </header>
         </div>
 
-        @foreach(session("notifications", []) as $notifs)
-            @php
-                echo $notifs->show();
-            @endphp
-        @endforeach
 
-        <main class="-mt-32">
-            <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6 z-50">
+        <div class="flex w-full flex-col items-center space-y-4 sm:items-end z-50">
+            @foreach(session("notifications", []) as $notifs)
+                @php
+                    echo $notifs->show();
+                @endphp
+            @endforeach
+        </div>
+    </div>
 
-                <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
 
-                    @if ($errors->any())
-                        <div class="rounded-md bg-red-50 p-4 mb-6">
-                            <div class="flex">
-                                <div class="flex-shrink-0">
-                                    <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor"
-                                         aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
-                                              clip-rule="evenodd"/>
-                                    </svg>
-                                </div>
-                                <div class="ml-3">
-                                    <h3 class="text-sm font-medium text-red-800">Nous avons
-                                        trouvé {{ count($errors->all()) }} erreurs :(</h3>
-                                    <div class="mt-2 text-sm text-red-700">
-                                        <ul role="list" class="list-disc space-y-1 pl-5">
-                                            @foreach ($errors->all() as $error)
-                                                <li>{{ $error }}</li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
+    <main class="-mt-32">
+        <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+            <div class="rounded-lg bg-white px-5 py-6 shadow sm:px-6">
+                @if ($errors->any())
+                    <div class="rounded-md bg-red-50 p-4 mb-6">
+                        <div class="flex">
+                            <div class="flex-shrink-0">
+                                <svg class="h-5 w-5 text-red-400" viewBox="0 0 20 20" fill="currentColor"
+                                     aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+                            </div>
+                            <div class="ml-3">
+                                <h3 class="text-sm font-medium text-red-800">Nous avons
+                                    trouvé {{ count($errors->all()) }} erreurs :(</h3>
+                                <div class="mt-2 text-sm text-red-700">
+                                    <ul role="list" class="list-disc space-y-1 pl-5">
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                @endif
 
-                    @endif
+                @yield("main")
 
-                    @yield("main")
-                </div>
             </div>
-        </main>
-    </div>
+        </div>
+    </main>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
